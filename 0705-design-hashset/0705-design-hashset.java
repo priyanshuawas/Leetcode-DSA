0c9e993661b7a5e4a83e68afb7601b6f87721cbd
Priyanshu Awasthi <80937942+priyanshuawas@users.noreply.github.com>
2023-05-30 11:03:59 +0530

    Time: 19 ms (50.28%), Space: 49.5 MB (79.91%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.java
@@ -0,0 +1,40 @@
+class MyHashSet {
+    private static final int NUM_BUCKETS = 1000;
+    private List<List<Integer>> buckets;
+    public MyHashSet() {
+        buckets = new ArrayList<>(NUM_BUCKETS);
+        for (int i = 0; i < NUM_BUCKETS; i++) 
+        {
+            buckets.add(new ArrayList<>());
+        }
+    }
+    
+    public void add(int key) {
+        int hash = key % NUM_BUCKETS;
+        List<Integer> bucket = buckets.get(hash);
+        if (!bucket.contains(key)) 
+        {
+            bucket.add(key);
+        }
+    }
+    
+    public void remove(int key) {
+        int hash = key % NUM_BUCKETS;
+        List<Integer> bucket = buckets.get(hash);
+        bucket.remove(Integer.valueOf(key));
+    }
+    
+    public boolean contains(int key) {
+        int hash = key % NUM_BUCKETS;
+        List<Integer> bucket = buckets.get(hash);
+        return bucket.contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
