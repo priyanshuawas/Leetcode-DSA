96fe22775160f1b4814b4bd8f51a2be1fc0277ec
Priyanshu Awasthi <80937942+priyanshuawas@users.noreply.github.com>
2023-09-13 22:41:21 +0530

    Time: 5 ms (100.00%), Space: 44.3 MB (72.37%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+
+        char[] arr = s.toCharArray();
+        int open = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == '(')
+                open++;
+            else if (arr[i] == ')') {
+                if (open == 0)
+                    arr[i] = '*';
+                else
+                    open--;
+            }
+        }
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (open > 0 && arr[i] == '(') {
+                arr[i] = '*';
+                open--;
+            }
+        }
+
+        int p = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != '*')
+                arr[p++] = arr[i];
+        }
+
+        return new String(arr).substring(0, p);
+    }
+}
